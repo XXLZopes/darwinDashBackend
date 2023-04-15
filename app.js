b558e7df44601b64611ccdb122f4c014319f502e
Aidan Meyer <afordm99@gmail.com>
2023-04-15 15:01:03 -0700

    finishing

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 const express = require("express");
 
-if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
-}
+// if (process.env.NODE_ENV !== "production") {
+//     require("dotenv").config();
+// }
 
 const app = express();
 app.use(express.json());
